cdabf4e112b1c4ea1ff8dc863ce55e5b942f48c0
shiryanshbhati7012 <shiryansh7012@gmail.com>
2023-10-11 20:49:00 +0530

    blogs

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
 
 import express from 'express';
 import bodyParser from 'body-parser';
+import dotenv from "dotenv";
 import mongoose from 'mongoose';
 import cors from 'cors';
-
 import postRoutes from './routes/posts.js';
 
 const app = express();
-
+dotenv.config();
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
+
 //app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
-
-
-
-//const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
 mongoose.connect("mongodb://127.0.0.1:27017/server")
-
-
-
 app.use('/posts', postRoutes);
-
-//mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
- // .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  //.catch((error) => console.log(`${error} did not connect`));
-
 //mongoose.set('useFindAndModify', false);
-app .listen(5000,()=>{console.log("server is running on port 5000")})
+
+app .listen(process.env.PORT,()=>{
+  console.log(`the server is running on port ${process.env.PORT}` )
+});
